4842381a11bf7322339d36ecc9d9241b5d95fc3e
diazzqlz <diazzqlz7@gmail.com>
2023-11-20 10:19:32 -0300

    first commit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,21 +3,21 @@
 import Cookie from 'js-cookie'
 import { useRouter } from 'next/navigation'
 
-export default function Home() {
+export default function Dashboard() {
   const router = useRouter()
 
-  function handleLogin() {
-    Cookie.set('auth_token', '1239k1231293129i319i2dkakdaskdoa')
-    router.push('/dashboard')
+  function handleLogout() {
+    Cookie.remove('auth_token')
+    router.push('/')
   }
 
   return (
     <div className='flex w-full h-screen justify-center items-center flex-col gap-3'>
       <button 
-        className='w-1/4 h-15 p-2 bg-green-600 rounded'
-        onClick={handleLogin}
+        className='w-1/4 h-15 p-2 bg-red-600 rounded'
+        onClick={handleLogout}
       >
-        Login
+        Logout
       </button>
     </div>
   )
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse} from 'next/server'
+
+export default function middleware(request: NextRequest, response: NextResponse) {
+  const token = request.cookies.get('auth_token')?.value
+
+  const signInURL = new URL('/', request.url)
+  const dashboardURL = new URL('/dashboard', request.url)
+
+  if(!token) {
+    if (request.nextUrl.pathname === '/') {
+      return NextResponse.next()
+    }
+    return NextResponse.redirect(signInURL)
+  }
+
+  if(request.nextUrl.pathname === '/') {
+    return NextResponse.redirect(dashboardURL)
+  }
+}
+
+export const config = {
+  matcher: ['/', '/dashboard/:path*']
+}
